ea209c8ef59c0cd73a98bc79b32bc5f9a798046d
dkreutz@t-online.de <dkreutz@t-online.de>
2019-01-03 11:50:37 +0100

    fixing install requirements

--- requirements.sh
+++ requirements.sh
@@ -2,5 +2,6 @@
 
 # mraa libs are now added in the local skill folder
 #sudo apt-get -y install python3-mraa=1.9.0 libmraa1=1.9.0
+sudo apt-get -y install python3-mraa libmraa1
 sudo usermod -a -G input respeaker
 exit 0
