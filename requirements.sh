e5b811e3882a97001b928d2809b0ae598109df83
Dominik <39655102+domcross@users.noreply.github.com>
2019-01-03 11:05:04 +0100

    Update requirements.sh
    mraa libs are local, no install necessary

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo apt-get -y install python3-mraa=1.9.0 libmraa1=1.9.0
+# mraa libs are now added in the local skill folder
+#sudo apt-get -y install python3-mraa=1.9.0 libmraa1=1.9.0
 exit 0
