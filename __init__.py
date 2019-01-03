5c75946c3cd5c8c8e3b8b5e5b98a171450d84bfc
dkreutz@t-online.de <dkreutz@t-online.de>
2019-01-03 11:41:21 +0100

    user button support

--- __init__.py
+++ __init__.py
@@ -1,12 +1,31 @@
+# Copyright 2018, domcross
+# Github https://github.com/domcross
+#
+# inspired by https://github.com/andlo/picroft-google-aiy-voicekit-skill.git
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import time
+from evdev import InputDevice
 from pixel_ring import pixel_ring
 from . import mraa
 import os
 from mycroft import MycroftSkill, intent_file_handler
+from mycroft.messagebus.message import Message
 from mycroft.util.log import LOG
 
 
-class RespeakerPixelRing(MycroftSkill):
+class RespeakerIo(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
@@ -20,6 +39,16 @@ class RespeakerPixelRing(MycroftSkill):
         self.en.write(0)
         pixel_ring.set_brightness(20)
         pixel_ring.wakeup()
+
+        self.userkey = None
+        try:
+            self.userkey = InputDevice("/dev/input/event0")
+        except Exception as e:
+            LOG.debug("exception while reading InputDevice: {}".format(e))
+
+        if self.userkey:
+            self.schedule_repeating_event(self.handle_button, None, 0.1,
+                                          'RespeakerIo')
 
         self.add_event('recognizer_loop:record_begin',
                        self.handle_listener_wakeup)
@@ -55,10 +84,27 @@ class RespeakerPixelRing(MycroftSkill):
         LOG.debug("off")
         pixel_ring.off()
 
+    def handle_button(self, message):
+        if not self.userkey:
+            return
+
+        longpress_threshold = 2
+        respeaker_userkey_code = 194
+
+        if respeaker_userkey_code in self.userkey.active_keys():
+            pressed_time = time.time()
+            while respeaker_userkey_code in self.userkey.active_keys():
+                time.sleep(0.2)
+            pressed_time = time.time()-pressed_time
+            if pressed_time < longpress_threshold:
+                self.bus.emit(Message("mycroft.mic.listen"))
+            else:
+                self.bus.emit(Message("mycroft.stop"))
+
     @intent_file_handler('ring.pixel.respeaker.intent')
     def handle_ring_pixel_respeaker(self, message):
         self.speak_dialog('ring.pixel.respeaker')
 
 
 def create_skill():
-    return RespeakerPixelRing()
+    return RespeakerIo()
--- requirements.sh
+++ requirements.sh
@@ -2,4 +2,5 @@
 
 # mraa libs are now added in the local skill folder
 #sudo apt-get -y install python3-mraa=1.9.0 libmraa1=1.9.0
+sudo usermod -a -G input respeaker
 exit 0
